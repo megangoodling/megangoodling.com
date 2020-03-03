9020dafabb70dfc06714c7dc2232a3ebb985e44a
megangoodling <megan.goodling@thoughtworks.com>
2020-03-03 12:55:34 -0600

    fix variable name for DB

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //connect to the database
-mongoose.connect(process.env.DB, { useNewUrlParser: true })
+mongoose.connect(MONGOLAB_URI, { useNewUrlParser: true })
   .then(() => console.log(`Database connected successfully`))
   .catch(err => console.log(err));
 
