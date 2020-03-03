a7b6ad5f51a41e9605fcc093a1542729e5a4f429
megangoodling <megan.goodling@thoughtworks.com>
2020-03-03 15:34:32 -0600

    fix node modules, install react-router, add Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import Todo from './components/Todo';
+import Home from './pages/Home/Home'
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
+    <BrowserRouter>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/todo' component={Todo} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
