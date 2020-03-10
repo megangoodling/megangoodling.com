06d4d18c8223a5d6afd80f2072b87982545adbec
megangoodling <megan.goodling@thoughtworks.com>
2020-03-10 12:01:16 -0500

    fix node modules and add some eslint rules

--- client/src/components/atoms/List/List.js
+++ client/src/components/atoms/List/List.js
@@ -17,32 +17,29 @@ class List extends React.Component {
   // Retrieves the list of items from the Express app
   getList = () => { // eslint-disable-line
     fetch('/api/getList')
-      .then(res => res.json())
-      .then(list => this.setState({ list }))
+      .then((res) => res.json())
+      .then((list) => this.setState({ list }))
   }
 
   render () {
     const { list } = this.state
 
     return (
-      <div className="App">
+      <div className='App'>
         <h2>List of Items</h2>
         {list.length ? (
           <div>
-            {list.map((item) => {
-              return (
-                <div key={list.indexOf(item)}>
-                  {item}
-                </div>
-              )
-            })}
+            {list.map((item) => (
+              <div key={list.indexOf(item)}>
+                {item}
+              </div>
+            ))}
           </div>
         ) : (
           <div>
             <h2>No List Items Found</h2>
           </div>
-        )
-        }
+        )}
       </div>
     )
   }
